f23fbac76c55c444e8c322a547d177841e0ddeed
David Irvin <davidirvin47@gmail.com>
2021-09-15 14:15:42 -0600

    Test connection to mongoDB

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express")
 const path = require("path")
 const connectDB = require("./db/db")
+const mongoose = require("mongoose")
 
 const app = express()
 
@@ -20,3 +21,21 @@ if (process.env.NODE_ENV === "production") {
 
     app.get("/", (req, res) => res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html')))
 }
+
+const testSchema = mongoose.Schema({
+    doc: {
+        type: String
+    }
+})
+
+const Test = mongoose.model("Test", testSchema)
+
+app.get("/testdb", async (req, res) => {
+    Test.create({doc: "This is from the DB"}, {new: true}, (err, newTest) => {
+        if (err) {
+            console.log(err);
+            res.status(400)
+        }
+        res.send(newTest)
+    })
+})
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,29 @@
 import './App.css';
+import react, { useEffect, useState } from 'react'
+import axios from "axios"
 
 function App() {
+  const [test, setTest] = useState("")
+
+  useEffect(() => {
+    testDB()
+  }, [])
+  
+  const testDB = async () => {
+    try {
+      const res = await axios.get("/testdb")
+      setTest(res.data.doc)
+      console.log(res);
+    } catch (error) {
+      console.log(error.response); 
+    }
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src="https://ca.slack-edge.com/T040ZFA3W-U01SGE9PCJJ-894fd36c293c-512" className="App-logo" alt="logo" />
         <p>LET'S GET EXCITED</p>
+        <p>This is from DB: {test}</p>
       </header>
     </div>
   );
