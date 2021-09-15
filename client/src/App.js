37c44c3e123dda5fe2fefa8cdf3c382ed7cda59f
David Irvin <davidirvin47@gmail.com>
2021-09-15 10:21:59 -0600

    Remove create-react-app bloat

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src="https://ca.slack-edge.com/T040ZFA3W-U01SGE9PCJJ-894fd36c293c-512" className="App-logo" alt="logo" />
+        <p>LET'S GET EXCITED</p>
       </header>
     </div>
   );
