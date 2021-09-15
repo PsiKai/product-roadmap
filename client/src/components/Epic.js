435809a7cb1a557ac25066ac1bced78f6ce4ec0c
David Irvin <davidirvin47@gmail.com>
2021-09-15 17:06:46 -0600

    Hookup form to backend and db for create functionality

--- client/src/components/Epic.js
+++ client/src/components/Epic.js
@@ -3,10 +3,10 @@ import React from 'react'
 const Epic = ({ epic: { title, toolkit, status, description } }) => {
     return (
         <div>
-            <p>{toolkit}</p>
-            <p>{title}</p>
+            <h1>{toolkit}</h1>
+            <h2>{title}</h2>
             <p>{description}</p>
-            <p>{status}</p>
+            <h3>{status}</h3>
         </div>
     )
 }
--- client/src/components/NewEpic.js
+++ client/src/components/NewEpic.js
@@ -1,28 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from "axios"
 
 const NewEpic = ({epics}) => {
+    const [form, setForm] = useState({})
+
+    const inputChange = (e) => {
+        setForm({
+            ...form,
+            [e.target.id]: e.target.value
+        })
+    }
+
+    const submitForm = async (e) => {
+        e.preventDefault()
+        const newEpic = {epic: form}
+        try {
+            const res = await axios.post("/epic/new", newEpic)
+            console.log(res.data);
+            window.location.reload()
+        } catch (error) {
+            console.log(error.response);
+        }
+    }
+
+    const toolkits = ["Borrower Toolkit", "Loan Officer Toolkit", "Manager and Integrations Toolkit"]
+    const statuses = ["Planned", "In Progress", "Completed", "Pruned"]
+
     return (
-        <form>
+        <form onSubmit={submitForm}>
             <label htmlFor="toolkit">Toolkit</label>
-            <input id="toolkit" type="text"></input>
+            <select onChange={inputChange} id="toolkit">
+                <option value="">Select Toolkit</option>
+                {toolkits.map((kit, i) => <option value={kit} key={i}>{kit}</option>)}
+            </select>
+
             <label htmlFor="title">Title</label>
-            <input id="title" type="text"></input>
+            <input onChange={inputChange} id="title" type="text"></input>
+
             <label htmlFor="description">Description</label>
-            <textarea id="description"></textarea>
+            <textarea onChange={inputChange} id="description"></textarea>
+
             <label htmlFor="status">Status</label>
-            <input id="status" type="text"></input>
+            <select onChange={inputChange} id="status">
+                <option value="">Select Status</option>
+                {statuses.map((status, i) => <option value={status} key={i}>{status}</option>)}
+            </select>
+
             <label htmlFor="priority">Priority</label>
-            <input id="priority" type="text"></input>
+            <input 
+                onChange={inputChange} 
+                id="priority" 
+                type="number" 
+                step="1" 
+                min="1"
+            >
+            </input>
+
             <label htmlFor="blocking">Blocking:</label>
-            <select id="blocking">
+            <select onChange={inputChange} id="blocking">
                 <option value="">None</option>
                 {epics.map((item) => <option key={item._id} value={item._id}>{item.title}</option>)}
             </select>
+
             <label htmlFor="blocked">Blocked By:</label>
-            <select id="blocked">
+            <select onChange={inputChange} id="blocked">
                 <option value="">None</option>
                 {epics.map((item) => <option key={item._id} value={item._id}>{item.title}</option>)}   
             </select>
+
             <button type="submit">Submit</button>
         </form>
     )
