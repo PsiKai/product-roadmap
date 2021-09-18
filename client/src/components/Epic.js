7df64114d07fb76504dc7a307b880246bbb28519
David Irvin <davidirvin47@gmail.com>
2021-09-17 20:54:40 -0600

    Change status to icons

--- client/src/components/Epic.js
+++ client/src/components/Epic.js
@@ -2,6 +2,12 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
+import EventAvailableIcon from '@mui/icons-material/EventAvailable';
+import BuildIcon from '@mui/icons-material/Build';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import ContentCutIcon from '@mui/icons-material/ContentCut';
+import BlockIcon from '@mui/icons-material/Block';
+import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
 
 
 const Epic = ({ epic: { title, status, description, dependencies, _id, toolkit }, edit }) => {
@@ -17,25 +23,38 @@ const Epic = ({ epic: { title, status, description, dependencies, _id, toolkit }
         }
     }
 
+    const statusIcon = (cardStatus) => {
+        switch (cardStatus) {
+            case "Planned": return <EventAvailableIcon/>
+            case "In Progress": return <BuildIcon/>
+            case "Completed": return <DoneAllOutlinedIcon/>
+            case "Pruned": return <ContentCutIcon/>
+            case "Blocked": return <BlockIcon/>
+            default: break;
+        }
+    }
+
     const direction = focus ? "grow" : "shrink"
     const height = {animation: `${direction} 400ms ease forwards`}
 
     return (
         <div className="epic__card" onClick={() => setFocus(!focus)}>
             <h3>{title}</h3>
+            {statusIcon(status)}
             <div className="hidden epic__card--info" style={height}>
                 <div className="epic__card--buttons">
                     <button type="button" id={_id} name={toolkit} onClick={edit} className="action"><EditIcon/></button>
                     <button type="button" id={_id} onClick={deleteEpic} className="action"><DeleteIcon/></button>
                 </div>
                 <p>{description}</p>
-                <h4>{status}</h4>
+                {/* <h4>{status}</h4> */}
                 <div className="epic__card--container">
                 {dependencies.map((dep, i) => {
                     return  <div key={dep.id} className="epic__card--dependency">
                                 <h4>{dep.title}</h4>
+                                {statusIcon(dep.status)}
                                 <p>{dep.description}</p>
-                                <h5>{dep.status}</h5>
+                                {/* <h5>{dep.status}</h5> */}
                             </div>
                 })}
                 </div>
--- client/src/components/Legend.js
+++ client/src/components/Legend.js
@@ -4,15 +4,29 @@ import BuildIcon from '@mui/icons-material/Build';
 import AssignmentTurnedInIcon from '@mui/icons-material/AssignmentTurnedIn';
 import BlockIcon from '@mui/icons-material/Block';
 import ContentCutIcon from '@mui/icons-material/ContentCut';
+import CheckCircleOutlineIcon from '@mui/icons-material/CheckCircleOutline';
+import BuildOutlinedIcon from '@mui/icons-material/BuildOutlined';
+import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
 
 const Legend = () => {
     return (
-        <div>
-            <EventAvailableIcon /> <span>Planned</span>
-            <BuildIcon/> <span>In Progress</span>
-            <BlockIcon /> <span>Blocked</span>
-            <ContentCutIcon/> <span>Pruned</span>
-            <AssignmentTurnedInIcon/> <span>Completed</span>
+        <div className="legend">
+            <h3>Legend</h3>
+            <div className="icon-container">
+                <EventAvailableIcon /> <span>Planned</span>
+            </div>
+            <div className="icon-container">
+                <BuildIcon/> <span>In Progress</span>
+            </div>
+            <div className="icon-container">
+                <BlockIcon /> <span>Blocked</span>
+            </div>
+            <div className="icon-container">
+                <ContentCutIcon/> <span>Pruned</span>
+            </div>
+            <div className="icon-container">
+                <DoneAllOutlinedIcon/> <span>Completed</span>
+            </div>
             
         </div>
     )
