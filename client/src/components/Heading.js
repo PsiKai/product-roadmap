414f72360143538c8c798e6d217af84bb732faa5
David Irvin <davidirvin47@gmail.com>
2021-09-20 13:32:05 -0600

    Convert logo from png to svg

--- client/src/components/Heading.js
+++ client/src/components/Heading.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import {ReactComponent as MaxwellLogo} from '../images/MaxwellLogo.svg'
 
 const Heading = () => {
     return (
         <div className="heading">
-            <img src="./images/white-logo.png" alt="Maxwell Logo" className="logo-heading" />
+            {/* <img src="./images/white-logo.png" alt="Maxwell Logo" className="logo-heading" /> */}
+            <MaxwellLogo/>
             <h1>Maxwell Product Roadmap</h1>
         </div>
     )
